81814032f5e8d335254b3c2f0ba75382fef43be1
sherm <e0957369@u.nus.edu>
2026-01-22 15:39:59 +0800

    Implement task class
    Add support for mark and unmark

--- src/main/java/Revel.java
+++ src/main/java/Revel.java
@@ -1,10 +1,13 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Revel {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] storedItems = new String[100];
+        Task[] storedTasks = new Task[100];
         int itemCount = 0;
+        int selectedNumber;
+        Task selectedTask;
         boolean exitLoop = false;
         String intro = """
                 ____________________________________________________________
@@ -25,16 +28,31 @@ public class Revel {
                     break;
                 case "list":
                     System.out.println(indent);
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < itemCount; i++) {
-                        System.out.println(storedItems[i]);
+                        System.out.println((i + 1) + "." + storedTasks[i].toString());
                     }
                     System.out.println(indent);
                     continue;
                 case "mark":
+                    selectedNumber = Integer.parseInt(input.split(" ")[1]);
+                    selectedTask = storedTasks[selectedNumber - 1];
+                    selectedTask.markAsDone();
+                    System.out.println(indent + "\n" + " Nice! I've marked this task as done:\n  "
+                    + selectedTask.toString() + "\n" + indent);
+                    continue;
+                case "unmark":
+                    selectedNumber = Integer.parseInt(input.split(" ")[1]);
+                    selectedTask = storedTasks[selectedNumber - 1];
+                    selectedTask.markAsUndone();
+                    System.out.println(indent + "\n" + " OK, I've marked this task as not done yet:\n  "
+                                       + selectedTask.toString() + "\n" + indent);
+                    continue;
 
                 default:
+                    Task newTask = new Task(input);
+                    storedTasks[itemCount++] = newTask;
                     System.out.println(indent + "\n" + "added: " + input + "\n" + indent);
-                    storedItems[itemCount] = ++itemCount + ": " + input;
             }
 
             if (exitLoop) {

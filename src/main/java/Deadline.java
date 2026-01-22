c552b906aac85f1005c90a100d4466400951da8d
sherm <e0957369@u.nus.edu>
2026-01-23 00:14:46 +0800

    Add support for error handling

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,12 @@
+public class Deadline extends Task {
+    protected String byDate;
+    public Deadline(String description, String byDate) {
+        super(description);
+        this.byDate = byDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + this.byDate + ")";
+    }
+}

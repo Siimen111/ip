644baa58f94faf8fc6ff086ad7475c32bc118281
sherm <e0957369@u.nus.edu>
2026-01-22 14:50:51 +0800

    Add support for echo

--- src/main/java/Revel.java
+++ src/main/java/Revel.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Revel {
     public static void main(String[] args) {
-        String logo = """
+        Scanner sc = new Scanner(System.in);
+        String intro = """
                 ____________________________________________________________
                  Hello! I'm Revel
                  What can I do for you?
                 ____________________________________________________________
-                 Bye. Hope to see you again soon!
-                ____________________________________________________________""";
-        System.out.println(logo);
+                """;
+        String indent = "____________________________________________________________";
+        System.out.println(intro);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(indent + "\n" + " Bye. Hope to see you again soon!\n" + indent);
+                break;
+            }
+
+            System.out.println(indent + "\n" + input + "\n" + indent);
+        }
     }
 }

de2e9788844beadd06b8c5e4655cd4adeac3adc2
sherm <e0957369@u.nus.edu>
2026-01-22 15:19:20 +0800

    Update command implementation

--- src/main/java/Revel.java
+++ src/main/java/Revel.java
@@ -5,6 +5,7 @@ public class Revel {
         Scanner sc = new Scanner(System.in);
         String[] storedItems = new String[100];
         int itemCount = 0;
+        boolean exitLoop = false;
         String intro = """
                 ____________________________________________________________
                  Hello! I'm Revel
@@ -16,21 +17,30 @@ public class Revel {
 
         while (true) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(indent + "\n" + " Bye. Hope to see you again soon!\n" + indent);
-                break;
-            }
-            if (input.equals("list")) {
-                System.out.println(indent);
-                for (int i = 0; i < itemCount; i++) {
-                    System.out.println(storedItems[i]);
-                }
-                System.out.println(indent);
-                continue;
+
+            switch (input.split(" ")[0]) {
+                case "bye":
+                    System.out.println(indent + "\n" + " Bye. Hope to see you again soon!\n" + indent);
+                    exitLoop = true;
+                    break;
+                case "list":
+                    System.out.println(indent);
+                    for (int i = 0; i < itemCount; i++) {
+                        System.out.println(storedItems[i]);
+                    }
+                    System.out.println(indent);
+                    continue;
+                case "mark":
+
+                default:
+                    System.out.println(indent + "\n" + "added: " + input + "\n" + indent);
+                    storedItems[itemCount] = ++itemCount + ": " + input;
             }
 
-            System.out.println(indent + "\n" + "added: " + input + "\n" + indent);
-            storedItems[itemCount] = ++itemCount + ": " + input;
+            if (exitLoop) {
+                break;
+            }
+
         }
     }
 }

c6da8aa22cbf66d4c6e6efdbcde40edf84e59b04
sherm <e0957369@u.nus.edu>
2026-01-22 14:39:38 +0800

    Add greeting

--- src/main/java/Revel.java
+++ src/main/java/Revel.java
@@ -1,10 +1,12 @@
 public class Revel {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = """
+                ____________________________________________________________
+                 Hello! I'm Revel
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________""";
+        System.out.println(logo);
     }
 }
